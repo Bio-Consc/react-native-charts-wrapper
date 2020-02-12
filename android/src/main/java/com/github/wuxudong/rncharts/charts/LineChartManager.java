3eae1afa35eb3681788347fa76edc9992375117b
Nikita Kolmogorov <backmeupplz@gmail.com>
2020-02-12 12:14:46 -0800

    Update LineChartManager.java

--- android/src/main/java/com/github/wuxudong/rncharts/charts/LineChartManager.java
+++ android/src/main/java/com/github/wuxudong/rncharts/charts/LineChartManager.java
@@ -8,6 +8,32 @@ import com.github.wuxudong.rncharts.data.DataExtract;
 import com.github.wuxudong.rncharts.data.LineDataExtract;
 import com.github.wuxudong.rncharts.listener.RNOnChartValueSelectedListener;
 import com.github.wuxudong.rncharts.listener.RNOnChartGestureListener;
+import android.content.Context;
+import android.util.AttributeSet;
+
+class ExpandedLineChart extends LineChart {
+    public ExpandedLineChart(Context context) {
+        super(context);
+    }
+
+    public ExpandedLineChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ExpandedLineChart(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public float getLowestVisibleX() {
+        return super.getLowestVisibleX() - 1200.0f;
+    }
+
+    @Override
+    public float getHighestVisibleX() {
+        return super.getHighestVisibleX() + 1200.0f;
+    }
+}
 
 public class LineChartManager extends BarLineChartBaseManager<LineChart, Entry> {
 
@@ -18,7 +44,7 @@ public class LineChartManager extends BarLineChartBaseManager<LineChart, Entry> 
 
     @Override
     protected LineChart createViewInstance(ThemedReactContext reactContext) {
-        LineChart lineChart =  new LineChart(reactContext);
+        LineChart lineChart =  new ExpandedLineChart(reactContext);
         lineChart.setOnChartValueSelectedListener(new RNOnChartValueSelectedListener(lineChart));
         lineChart.setOnChartGestureListener(new RNOnChartGestureListener(lineChart));
         return lineChart;
